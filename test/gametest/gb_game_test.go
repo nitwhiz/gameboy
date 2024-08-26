1eca4c8883a74943f0b450885621e7c021e0ea7f
nitwhiz <schonfeldandy@gmail.com>
2024-08-26 23:38:45 +0200

    add gametest benchmark

--- test/gametest/gb_game_test.go
+++ test/gametest/gb_game_test.go
@@ -6,14 +6,13 @@ import (
 	"github.com/nitwhiz/gameboy/pkg/inst"
 	"os"
 	"testing"
-	"time"
 )
 
-func TestGame(t *testing.T) {
+func getGameBoy(b *testing.B) *gb.GameBoy {
 	rom, err := os.ReadFile("../../testdata/roms/custom/gametest.gb")
 
 	if err != nil {
-		t.Fatal(err)
+		b.Fatal(err)
 	}
 
 	inst.InitHandlers()
@@ -24,19 +23,28 @@ func TestGame(t *testing.T) {
 	)
 
 	if err != nil {
-		t.Fatal(err)
+		b.Fatal(err)
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	return g
+}
 
-	defer cancel()
+func BenchmarkGame(b *testing.B) {
+	b.ReportAllocs()
 
-	for {
-		select {
-		case <-ctx.Done():
-			return
-		default:
-			g.Update(ctx)
-		}
+	ctx := context.Background()
+	g := getGameBoy(b)
+
+	framesRendered := 0
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		g.Update(ctx)
+		framesRendered++
 	}
+
+	b.StopTimer()
+
+	b.ReportMetric(float64(framesRendered)/b.Elapsed().Seconds(), "fps")
 }
